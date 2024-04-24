ea5044c281ffd742c3d34bf00a9819863fdedca5
Juan Torres <juan.torres@pctclm.com>
2024-04-24 09:15:16 +0200

    Starting to refactor exercises endpoint

--- src/endpoints/exercises/exercises.test.js
+++ src/endpoints/exercises/exercises.test.js
@@ -1,21 +1,4 @@
-
-// Needed to access environment variables and for server not to crash.
-// if not included, then test will fail due to supertest not being able
-// to read EXPRESS_SESSION_SECRET
-require('dotenv').config();
-const supertest = require('supertest');
-const createApp = require('../../app.js');
-
-// true means that it should connect to test db
-const app = createApp(true);
-const BASE_ENDPOINT = '/exercises'
-
-function logErrors (err, req, res, next) {
-  console.error(err.stack)
-  next(err)
-}
-
-const request = supertest(app.use(logErrors))
+const { request, BASE_ENDPOINT } = require('./testsSetup');
 
 // Empty database before starting tests
 const truncateExercisesAndRelatedTables = async () => {
