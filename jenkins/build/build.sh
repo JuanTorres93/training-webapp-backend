c0d6ba7610bd3f76b0fb00b0acba2b22d4aa673d
Juan Torres <juan93git@gmail.com>
2024-10-14 12:25:17 +0200

    Starting tests to include scripts in jenkins pipeline.

---
+++ jenkins/build/build.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# TODO Create global directory for jenkins_home and system env variable with the path
+WORKSPACE=/home/juan/hdd/Cursos/Jenkins/jenkins-data/jenkins_home/workspace/trackoverload-backend-pipeline
+
+echo "Changing to workspace directory: $WORKSPACE"
+cd $WORKSPACE
+
+echo "Installing dependencies"
+docker run --rm --name npm npm install
