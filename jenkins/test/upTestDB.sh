063155d04b3b858a06bb647e96659089cbd0af20
Juan Torres <juan93git@gmail.com>
2024-10-15 08:15:15 +0200

    Create docker network if not exists

--- jenkins/test/upTestDB.sh
+++ jenkins/test/upTestDB.sh
@@ -4,6 +4,15 @@
 # TODO Create global directory for jenkins_home and system env variable with the path
 WORKSPACE=/home/juan/hdd/webapps/trackoverload/jenkins_home/workspace/trackoverload-backend-pipeline
 
+# Check if the Docker network exists
+if docker network ls --filter "name=^${JENKINS_DOCKER_TEST_NET}$" --format "{{.Name}}" | grep -wq "${JENKINS_DOCKER_TEST_NET}"; then
+    echo "Network '${JENKINS_DOCKER_TEST_NET}'  exists."
+else
+    echo "Network '${JENKINS_DOCKER_TEST_NET}' does not exist. Creating it..."
+    docker network create "${JENKINS_DOCKER_TEST_NET}"
+    echo "Network '${JENKINS_DOCKER_TEST_NET}' created."
+fi
+
 echo "***********************"
 echo "Launching test database"
 echo "***********************"
