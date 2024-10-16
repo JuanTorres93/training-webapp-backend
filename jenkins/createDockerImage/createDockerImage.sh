1e6357ab4ecb25af3b0cde185130795e5c73e941
Juan Torres <juan93git@gmail.com>
2024-10-16 09:53:57 +0200

    First try to make jenkins build the docker image

---
+++ jenkins/createDockerImage/createDockerImage.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+echo "*********************"
+echo "Creating docker image"
+echo "*********************"
+
+cd /var/jenkins_home/workspace/trackoverload-backend-pipeline
+
+cat ${TRACKOVERLOAD_BACKEND_DOCKERFILE_CONTENT} > Dockerfile
+
+docker build \
+            -t kashyr26/trackoverload-backend:$BUILD_NUMBER \
+            .
+
+# IMPORTANT!!!: DO NOT INCLUDE ANY COMMAND AFTER THIS LINE. IT COULD ALTER THE EXIT CODE OF THE SCRIPT AND,
+# HENCE, LEAD TO A WRONG RESULT IN THE JENKINS JOB. MORE PRECISELY, THE PIPELINE COULD CONTINUE EVEN IF
+# TESTS FAIL.
