93e7191120ef6e5f67f84fd42849d008bd8de5b0
Juan Torres <juan93git@gmail.com>
2024-10-17 12:58:50 +0200

    Test commit. kubectl mounted as volume in /root/.kube/config. Not sure if this will work due to permissions.

--- jenkins/deploy/deploy.sh
+++ jenkins/deploy/deploy.sh
@@ -8,7 +8,6 @@ echo "**********************"
 # Specifically, this is used to get the value of $BUILD_NUMBER
 envsubst < $KUBERNETES_CONFIG_PATH/backend-deployment.yml > /tmp/backend-deployment.yml
 
-# TODO NEXT connect kubernetes to the cluster
 kubectl apply -f /tmp/backend-deployment.yml -f $KUBERNETES_CONFIG_PATH/backend-service.yml
 
 # IMPORTANT!!!: DO NOT INCLUDE ANY COMMAND AFTER THIS LINE. IT COULD ALTER THE EXIT CODE OF THE SCRIPT AND,
