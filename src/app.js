8c4d226090ad6db1f2afb0824151fcfafac77eba
Juan Torres <juan93git@gmail.com>
2025-04-02 07:14:17 +0200

    Refactor body parsing middleware to exclude webhook endpoint

--- src/app.js
+++ src/app.js
@@ -31,7 +31,17 @@ const createApp = () => {
   );
 
   // Parse HTTP request body to JSON
-  app.use(express.json());
+  // app.use(express.json());
+
+  // Apply body parser middleware to all requests except for the webhook
+  const jsonParser = bodyParser.json();
+  app.use((req, res, next) => {
+    if (req.originalUrl === "/webhook-checkout") {
+      next();
+    } else {
+      jsonParser(req, res, next);
+    }
+  });
 
   // Routers imports
   const usersRouter = require("./endpoints/users/users.js");
