ad9aee402ac6724b821f8c818798ec7c724d7fb3
Juan Torres <juan93git@gmail.com>
2025-04-04 07:58:51 +0200

    Include expiration date of free trial on user creation

--- src/db/users.js
+++ src/db/users.js
@@ -1,252 +1,324 @@
-const { query } = require('./index');
-const qh = require('./queryHelper.js');
-const { selectFreeTrialSubscription } = require('./subscriptions.js');
+const { query } = require("./index");
+const qh = require("./queryHelper.js");
+const { selectFreeTrialSubscription } = require("./subscriptions.js");
+const { createPayment } = require("./payments.js");
 
-const TABLE_NAME = 'users';
-const SELECT_USER_FIELDS = 'id, username, email, subscription_id, last_name, img, second_last_name, is_premium, is_early_adopter, created_at';
+const TABLE_NAME = "users";
+const SELECT_USER_FIELDS =
+  "id, username, email, subscription_id, last_name, img, second_last_name, is_premium, is_early_adopter, created_at";
 
 const checkStringInFieldInUse = async (field, value) => {
-    const q = "SELECT " + field +
-        " FROM " + TABLE_NAME + " WHERE LOWER(" + field + ") = LOWER($1);";
-    const params = [value];
+  const q =
+    "SELECT " +
+    field +
+    " FROM " +
+    TABLE_NAME +
+    " WHERE LOWER(" +
+    field +
+    ") = LOWER($1);";
+  const params = [value];
 
-    // Must return a promise to be able to await when calling from another file
-    return new Promise((resolve, reject) => {
-        query(q, params, (error, results) => {
-            if (error) reject({
-                error,
-                exists: null,
-            });
-
-            if (results !== undefined && results.rows.length > 0) {
-                resolve(true);
-            } else {
-                reject({
-                    error: null,
-                    exists: false,
-                });
-            }
+  // Must return a promise to be able to await when calling from another file
+  return new Promise((resolve, reject) => {
+    query(q, params, (error, results) => {
+      if (error)
+        reject({
+          error,
+          exists: null,
         });
+
+      if (results !== undefined && results.rows.length > 0) {
+        resolve(true);
+      } else {
+        reject({
+          error: null,
+          exists: false,
+        });
+      }
     });
+  });
 };
 
 const checkEmailInUse = async (email) => {
-    try {
-        // checkStringInFieldInUse only resolves to true
-        const exists = await checkStringInFieldInUse('email', email);
+  try {
+    // checkStringInFieldInUse only resolves to true
+    const exists = await checkStringInFieldInUse("email", email);
 
-        return exists
-    } catch (error) {
-        if (error.error !== null) throw error;
+    return exists;
+  } catch (error) {
+    if (error.error !== null) throw error;
 
-        return false;
-    }
+    return false;
+  }
 };
 
 const checkAliasInUse = async (username) => {
-    try {
-        // checkStringInFieldInUse only resolves to true
-        return await checkStringInFieldInUse('username', username);
-    } catch (error) {
-        if (error.error !== null) throw error;
+  try {
+    // checkStringInFieldInUse only resolves to true
+    return await checkStringInFieldInUse("username", username);
+  } catch (error) {
+    if (error.error !== null) throw error;
 
-        return false;
-    }
+    return false;
+  }
 };
 
 const registerNewUser = async ({
+  username,
+  email,
+  password,
+  is_premium,
+  is_early_adopter,
+  created_at,
+  subscription_id,
+  oauth_registration,
+}) => {
+  // Build query
+  let requiredFields = [
+    "username",
+    "email",
+    "password",
+    "is_premium",
+    "is_early_adopter",
+    "created_at",
+  ];
+  let requiredValues = [
     username,
     email,
     password,
     is_premium,
     is_early_adopter,
     created_at,
-    subscription_id,
-    oauth_registration,
-}) => {
+  ];
 
-    // Build query
-    let requiredFields = ['username', 'email', 'password', 'is_premium', 'is_early_adopter', 'created_at'];
-    let requiredValues = [username, email, password, is_premium, is_early_adopter, created_at];
+  let optionalFields = ["subscription_id", "oauth_registration"];
 
-    let optionalFields = ['subscription_id', 'oauth_registration'];
+  let subsId;
+  if (!subscription_id) {
+    subsId = await selectFreeTrialSubscription();
+    subsId = subsId.id;
+  } else {
+    subsId = subscription_id;
+  }
 
-    let subsId;
-    if (!subscription_id) {
-        subsId = await selectFreeTrialSubscription();
-        subsId = subsId.id;
-    } else {
-        subsId = subscription_id;
-    }
+  const oauth = oauth_registration ? oauth_registration : null;
+  let optionalValues = [subsId, oauth];
 
-    const oauth = oauth_registration ? oauth_registration : null;
-    let optionalValues = [subsId, oauth];
+  let returningFields = [
+    "id",
+    "username",
+    "email",
+    "subscription_id",
+    "last_name",
+    "img",
+    "second_last_name",
+    "is_premium",
+    "is_early_adopter",
+    "created_at",
+  ];
 
-    let returningFields = ['id', 'username', 'email', 'subscription_id', 'last_name', 'img', 'second_last_name', 'is_premium', 'is_early_adopter', 'created_at'];
+  const { q, params } = qh.createInsertIntoTableStatement(
+    TABLE_NAME,
+    requiredFields,
+    requiredValues,
+    optionalFields,
+    optionalValues,
+    returningFields
+  );
 
-    const { q, params } = qh.createInsertIntoTableStatement(TABLE_NAME,
-        requiredFields, requiredValues,
-        optionalFields, optionalValues,
-        returningFields);
+  return new Promise((resolve, reject) => {
+    query(q, params, (error, results) => {
+      if (error) reject(error);
 
-    return new Promise((resolve, reject) => {
-        query(q, params, (error, results) => {
-            if (error) reject(error);
+      const createdUser = results.rows[0];
 
-            const createdUser = results.rows[0];
-            resolve(createdUser)
-        })
+      const endFreeTrialDate = new Date();
+      // Add 1 month to the current date
+      endFreeTrialDate.setMonth(endFreeTrialDate.getMonth() + 1);
+      createPayment({
+        userId: createdUser.id,
+        subscriptionId: subsId,
+        amountInEur: 0,
+        nextPaymentDate: endFreeTrialDate,
+      });
+      resolve(createdUser);
     });
-}
+  });
+};
 
 const selectAllUsers = () => {
-    const q = "SELECT " + SELECT_USER_FIELDS + " FROM " + TABLE_NAME + ";";
-    const params = [];
+  const q = "SELECT " + SELECT_USER_FIELDS + " FROM " + TABLE_NAME + ";";
+  const params = [];
 
-    return new Promise((resolve, reject) => {
-        query(q, params, (error, results) => {
-            if (error) reject(error);
-            const users = results.rows;
-            resolve(users)
-        })
+  return new Promise((resolve, reject) => {
+    query(q, params, (error, results) => {
+      if (error) reject(error);
+      const users = results.rows;
+      resolve(users);
     });
+  });
 };
 
 const selectUserById = async (id) => {
-    const q = "SELECT " + SELECT_USER_FIELDS + " FROM " +
-        TABLE_NAME + " WHERE id = $1;";
-    const params = [id];
+  const q =
+    "SELECT " + SELECT_USER_FIELDS + " FROM " + TABLE_NAME + " WHERE id = $1;";
+  const params = [id];
 
-    return new Promise((resolve, reject) => {
-        query(q, params, (error, results) => {
-            if (error) reject(error);
-            const user = results.rows[0];
-            resolve(user)
-        })
+  return new Promise((resolve, reject) => {
+    query(q, params, (error, results) => {
+      if (error) reject(error);
+      const user = results.rows[0];
+      resolve(user);
     });
+  });
 };
 
 const selectUserByEmail = async (email) => {
-    const q = "SELECT " + SELECT_USER_FIELDS + " FROM " +
-        TABLE_NAME + " WHERE email = $1;";
-    const params = [email];
+  const q =
+    "SELECT " +
+    SELECT_USER_FIELDS +
+    " FROM " +
+    TABLE_NAME +
+    " WHERE email = $1;";
+  const params = [email];
 
-    return new Promise((resolve, reject) => {
-        query(q, params, (error, results) => {
-            if (error) return reject(error);
-            const user = results.rows[0];
-            resolve(user)
-        })
+  return new Promise((resolve, reject) => {
+    query(q, params, (error, results) => {
+      if (error) return reject(error);
+      const user = results.rows[0];
+      resolve(user);
     });
+  });
 };
 
 const selectUserRegisteredByOAuth = async (email) => {
-    const q = "SELECT oauth_registration FROM " +
-        TABLE_NAME + " WHERE email = $1;";
-    const params = [email];
+  const q =
+    "SELECT oauth_registration FROM " + TABLE_NAME + " WHERE email = $1;";
+  const params = [email];
 
-    return new Promise((resolve, reject) => {
-        query(q, params, (error, results) => {
-            if (error) reject(error);
-            const oauthRegistration = results.rows[0].oauth_registration;
-            resolve(oauthRegistration)
-        })
+  return new Promise((resolve, reject) => {
+    query(q, params, (error, results) => {
+      if (error) reject(error);
+      const oauthRegistration = results.rows[0].oauth_registration;
+      resolve(oauthRegistration);
     });
+  });
 };
 
-
 const updateUser = async (id, userObject) => {
-    let returningFields = ['id', 'username', 'email', 'last_name', 'img', 'second_last_name'];
+  let returningFields = [
+    "id",
+    "username",
+    "email",
+    "last_name",
+    "img",
+    "second_last_name",
+  ];
 
-    const { q, params } = qh.createUpdateTableStatement(TABLE_NAME, id,
-        userObject,
-        returningFields)
+  const { q, params } = qh.createUpdateTableStatement(
+    TABLE_NAME,
+    id,
+    userObject,
+    returningFields
+  );
 
-    return new Promise((resolve, reject) => {
-        query(q, params, (error, results) => {
-            if (error) reject(error);
+  return new Promise((resolve, reject) => {
+    query(q, params, (error, results) => {
+      if (error) reject(error);
 
-            const updatedUser = results.rows[0];
-            resolve(updatedUser)
-        })
+      const updatedUser = results.rows[0];
+      resolve(updatedUser);
     });
-}
+  });
+};
 
 const deleteUser = async (id) => {
-    let q = "DELETE FROM " + TABLE_NAME + " WHERE id = $1 " +
-        "RETURNING id, username, email, last_name, img, second_last_name;";
-    const params = [id]
+  let q =
+    "DELETE FROM " +
+    TABLE_NAME +
+    " WHERE id = $1 " +
+    "RETURNING id, username, email, last_name, img, second_last_name;";
+  const params = [id];
 
-    return new Promise((resolve, reject) => {
-        query(q, params, (error, results) => {
-            if (error) reject(error);
+  return new Promise((resolve, reject) => {
+    query(q, params, (error, results) => {
+      if (error) reject(error);
 
-            const deletedUser = results.rows[0];
-            resolve(deletedUser)
-        })
+      const deletedUser = results.rows[0];
+      resolve(deletedUser);
     });
-}
+  });
+};
 
 const truncateTableTest = () => {
-    const appIsBeingTested = process.env.NODE_ENV === 'test';
+  const appIsBeingTested = process.env.NODE_ENV === "test";
 
-    // Only allow truncating table in test environment
-    if (!appIsBeingTested) {
-        return new Promise((resolve, reject) => {
-            // Test for making malicious people think they got something
-            resolve('Truncated ' + TABLE_NAME);
-        });
-    }
-
-    const q = "TRUNCATE " + TABLE_NAME + " CASCADE;";
-    const params = [];
-
+  // Only allow truncating table in test environment
+  if (!appIsBeingTested) {
     return new Promise((resolve, reject) => {
-        query(q, params, (error, results) => {
-            if (error) {
-                reject(error)
-            };
-
-
-            resolve('Table ' + TABLE_NAME + ' truncated in test db.')
-        }, true)
+      // Test for making malicious people think they got something
+      resolve("Truncated " + TABLE_NAME);
     });
+  }
+
+  const q = "TRUNCATE " + TABLE_NAME + " CASCADE;";
+  const params = [];
+
+  return new Promise((resolve, reject) => {
+    query(
+      q,
+      params,
+      (error, results) => {
+        if (error) {
+          reject(error);
+        }
+
+        resolve("Table " + TABLE_NAME + " truncated in test db.");
+      },
+      true
+    );
+  });
 };
 
 const testDbSelectEverythingFromUserId = (id) => {
-    const appIsBeingTested = process.env.NODE_ENV === 'test';
+  const appIsBeingTested = process.env.NODE_ENV === "test";
 
-    // Only allow truncating table in test environment
-    if (!appIsBeingTested) {
-        return new Promise((resolve, reject) => {
-            // Test for making malicious people think they got something
-            resolve('Nothing in ' + TABLE_NAME);
-        });
-    }
-
-    const q = "SELECT * FROM " + TABLE_NAME + " WHERE id = $1;";
-    const params = [id];
-
+  // Only allow truncating table in test environment
+  if (!appIsBeingTested) {
     return new Promise((resolve, reject) => {
-        query(q, params, (error, results) => {
-            if (error) reject(error);
-
-            resolve(results.rows[0]);
-        }, true);
+      // Test for making malicious people think they got something
+      resolve("Nothing in " + TABLE_NAME);
     });
+  }
+
+  const q = "SELECT * FROM " + TABLE_NAME + " WHERE id = $1;";
+  const params = [id];
+
+  return new Promise((resolve, reject) => {
+    query(
+      q,
+      params,
+      (error, results) => {
+        if (error) reject(error);
+
+        resolve(results.rows[0]);
+      },
+      true
+    );
+  });
 };
 
 module.exports = {
-    checkStringInFieldInUse,
-    checkEmailInUse,
-    checkAliasInUse,
-    registerNewUser,
-    selectAllUsers,
-    selectUserById,
-    selectUserByEmail,
-    selectUserRegisteredByOAuth,
-    updateUser,
-    deleteUser,
-    truncateTableTest,
-    testDbSelectEverythingFromUserId,
+  checkStringInFieldInUse,
+  checkEmailInUse,
+  checkAliasInUse,
+  registerNewUser,
+  selectAllUsers,
+  selectUserById,
+  selectUserByEmail,
+  selectUserRegisteredByOAuth,
+  updateUser,
+  deleteUser,
+  truncateTableTest,
+  testDbSelectEverythingFromUserId,
 };
