0a1ac03009c555619a52ea22a4869a7689e14aca
Juan Torres <juan93git@gmail.com>
2025-05-08 08:19:44 +0200

    Refactor error handling functions for improved clarity and separation of concerns (dev vs prod)

--- src/controllers/errorController.js
+++ src/controllers/errorController.js
@@ -1,9 +1,47 @@
+const AppError = require("../utils/appError");
+
+const sendErrorDev = (err, res) => {
+  res.status(err.statusCode).json({
+    status: err.status,
+    error: err,
+    message: err.message,
+    stack: err.stack,
+  });
+};
+
+const sendErrorProd = (err, res) => {
+  // Operational, trusted error: send message to client
+  if (err.isOperational) {
+    res.status(err.statusCode).json({
+      status: err.status,
+      message: err.message,
+    });
+  }
+
+  // Programming or other unknown error: don't leak error details
+  else {
+    // 1) Log error
+    console.error("ERROR 💥", err);
+
+    // 2) Send generic message
+    res.status(500).json({
+      status: "error",
+      message: "Something went wrong.",
+    });
+  }
+};
+
 module.exports = (err, req, res, next) => {
   err.statusCode = err.statusCode || 500;
   err.status = err.status || "error";
 
-  res.status(err.statusCode).json({
-    status: err.status,
-    message: err.message,
-  });
+  if (process.env.NODE_ENV !== "production") {
+    // DEVELOPMENT AND TEST ENVIRONMENTS
+    sendErrorDev(err, res);
+  } else {
+    // PRODUCTION ENVIRONMENT
+    let error = { ...err, name: err.name };
+
+    sendErrorProd(error, res);
+  }
 };
