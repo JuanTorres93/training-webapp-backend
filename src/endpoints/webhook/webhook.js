57157c02df2f8433fecad5917137709238f56729
Juan Torres <juan93git@gmail.com>
2025-05-17 13:05:54 +0200

    Minor change to check for body alterations

--- src/endpoints/webhook/webhook.js
+++ src/endpoints/webhook/webhook.js
@@ -4,13 +4,13 @@ const paymentController = require("../../controllers/paymentController");
 const webhookApp = express();
 webhookApp.post(
   "/webhook-checkout",
+  // Parse the body
+  express.raw({ type: "application/json" }),
   (req, res, next) => {
     // TODO DELETE THESE DEBUG LOGS
     console.log("REACHES WEBHOOK in new ROUTER");
     next();
   },
-  // Parse the body
-  express.raw({ type: "application/json" }),
   paymentController.webhookCheckout
 );
 
